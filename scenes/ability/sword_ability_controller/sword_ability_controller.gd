2e6c384bae0f65a3e0ab45758caacad8a5130501
Xephis <stephenrichardking@gmail.com>
2024-04-14 09:31:49 +0100

    Spawn sword ability on enemies

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -1,5 +1,6 @@
 extends Node
 
+const MAX_RANGE = 150
 @export var sword_ability: PackedScene
 
 # Called when the node enters the scene tree for the first time.
@@ -7,10 +8,27 @@ func _ready():
 	pass
 
 func _on_timer_timeout():
+	# get the player and enemies
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
-		
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	
+	# filter enemies that are further than MAX_RANGE away.
+	enemies = enemies.filter(func(enemy: Node2D):
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2)
+	)
+	if enemies.size() == 0:
+		return
+	
+	# get the closest enemy
+	enemies.sort_custom(func(a: Node2D, b: Node2D):
+		var a_distance = a.global_position.distance_squared_to(player.global_position)
+		var b_distance = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance
+	)
+	
+	#instantiate sword at enemy[0] in the array of enemies (closest)
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player.global_position
+	sword_instance.global_position = enemies[0].global_position
